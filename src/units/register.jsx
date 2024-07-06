fb86da679cbd0b675ac3560cdc474be957bf5a10
naajiyanaushad <naajiyanaushad@gmail.com>
2024-07-06 12:06:44 +0530

    Add files via upload

--- src/units/register.jsx
+++ src/units/register.jsx
@@ -11,23 +11,14 @@ function Register()
         <div style={{border: '5px solid silver',display: 'flex',flexDirection: 'column',justifyContent: 'center',backgroundColor: 'palevioletred',borderRadius: '7px',gap: '5px',width: '300px',height: '500px'}}>
                         <h1 style={{fontStyle: 'oblique',color: 'white',height: '50px'}}>Register</h1>
                         <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
-          <div>
-            <label htmlFor="username" style={{
-              display: 'block',
-              marginBottom: '0.25rem',
-              fontSize: '0.875rem',
-              fontWeight: '500',
-              color: '#374151',}}>Username</label>
+        
             <div style={{display: 'flex',flexDirection: 'column',gap: '20px'}}>
             <input style={{border: '2px solid gray',borderRadius: '10px',height: '30px'}} type="text" id="Username" ></input>
             <input style={{border: '2px solid gray',borderRadius: '10px',height: '30px'}} type="text" id="Email"></input>
             <input style={{border: '2px solid gray',borderRadius: '10px',height: '30px'}} type="text" id="Password"></input>
             <input style={{border: '2px solid gray',borderRadius: '10px',height: '30px'}} type="text" id="Re-password"></input>
-            {/* onFocus={(e) => e.target.style.boxShadow = '0 0 0 2px #3b82f6'}
-              onBlur={(e) => e.target.style.boxShadow = '0 0 0 2px transparent'} /> */}
         <button style={{height: '50px', fontStyle: 'italic',color: 'grey',backgroundColor: 'white',borderRadius: '10px'}}>SIGN UP</button>
         <a href="/login" style={{fontStyle: 'italiC',color:' blue',display: 'flex',flexDirection: 'column'}}>Or LOGIN</a>
-        </div>
         </div>
         </div>
         </div>
